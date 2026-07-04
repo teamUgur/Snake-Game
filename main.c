24057ff5eecd3b2de7c2e651ceab2a45c1a8eb47
Kubinsky <166978514+teamUgur@users.noreply.github.com>
2026-07-04 22:39:31 +0300

    if not win

--- main.c
+++ main.c
@@ -21,3 +21,38 @@ int TailLen;
 int x, y, fruitx, fruity;
 int speed;
 
+#ifndef _WIN32
+struct termios origTermios;
+
+void disableRowMode() {
+    tcsetattr(STDIN_FILENO, TCSANOW, &origTermios);
+}
+
+void enableRowMode() {
+    tcgetattr(STDIN_FILENO, &origTermios);
+    struct termios raw = origTermios;
+    raw.c_lflag &= ~ (ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &raw);
+    atexit(disableRowMode);
+}
+
+int _kbhit() {
+    struct timeval tv = {0, 0};
+    fd_set fds;
+    FD_ZERO(&fds);
+    FD_SET(STDIN_FILENO, &fds);
+    select(STDIN_FILENO + 1, &fds, NULL, NULL, &tv);
+    return FD_ISSET(STDIN_FILENO, &fds);
+}
+
+int _getch() {
+    char c;
+    read(STDIN_FILENO, &c, 1);
+    return c;
+}
+
+void Sleep(int ms) {
+    usleep(ms * 1000);
+}
+
+#endif
